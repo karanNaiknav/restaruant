e6556e9eefe6b3acb814ce0643bff4c37ed74a80
Karan <karan.naiknav@gmail.com>
2022-04-04 20:10:35 -0400

    class

--- learn/learn.js
+++ learn/learn.js
@@ -3,3 +3,19 @@ y=2;
 
 z=x+y;
 console.log(z);
+
+
+var p1 = {
+    name: 'Karan',
+    age: 16
+}
+
+var p2 ={
+    name: 'karan', age: 16
+}
+
+var persons= [p1,p2];
+
+console.log(persons);
+
+persons.map
